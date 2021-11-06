9c6d09482db1d5590e4f6528205d8e9da69296b8
dortegas <90233528+dortegas@users.noreply.github.com>
2021-11-06 00:31:30 -0600

    Update Employee_Database_challenge.sql

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -14,10 +14,10 @@ WHERE e.birth_date BETWEEN '1952-01-01' AND '1955-12-31'
 ORDER BY e.emp_no;
 
 -- Use Dictinct with Orderby to remove duplicate rows
-SELECT DISTINCT ON (emp_no) emp_no,
-first_name,
-last_name,
-title
+SELECT DISTINCT ON (emp_no) emp_no
+  ,first_name
+  ,last_name
+  ,title
 INTO unique_titles
 FROM retirement_titles
 ORDER BY emp_no, to_date DESC;
